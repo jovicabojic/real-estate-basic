7920bcfbaf984879a193298bcdc11934669be6b0
jovicabojic <bojic.jovica@hotmail.com>
2023-09-19 10:29:30 +0200

    Finishing app

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,6 @@ export default {
     };
   },
   computed: {
-    // Convert coordinates based on the publication's address
     convertedCoordinates() {
       return (publication) => {
         if (publication.address && publication.address.coordinates) {
@@ -50,7 +49,6 @@ export default {
       try {
         const response = await axios.get('https://blattcodeservices.com/pct/search');
         this.publications = response.data.publications;
-        // Create custom markers and bind popups on mouseover
         this.createCustomMarkers();
       } catch (error) {
         console.error('Error fetching publications:', error);
@@ -63,48 +61,58 @@ export default {
           iconSize: [30, 30],
         });
         const marker = L.marker(this.convertedCoordinates(publication), {
-          icon: customIcon, // Assign the custom icon to the marker
+          icon: customIcon,
         });
-        marker.addTo(this.$refs.map.mapObject); // Add the marker to the map
+        marker.addTo(this.$refs.map.mapObject);
 
-        // Bind popup on mouseover
         marker.bindPopup(this.createPopupContent(publication), {
           keepInView: true,
           maxHeight: 400,
           offset: [0, -10],
         });
-
-        // Add mouseover and mouseout event listeners to show and hide the popup
         marker.on("mouseover", () => {
           marker.openPopup();
         });
-
-        // marker.on("mouseout", () => {
-        //   marker.closePopup();
-        // });
       });
     },
     createPopupContent(publication) {
       const maxDescriptionLength = 100;
-      const truncatedDescription = publication.description.length > maxDescriptionLength
-          ? `${publication.description.slice(0, maxDescriptionLength)}...`
-          : publication.description;
+      const truncatedDescription =
+          publication.description.length > maxDescriptionLength
+              ? `${publication.description.slice(0, maxDescriptionLength)}...`
+              : publication.description;
 
-      // Check if there are pictures in the publication, and use the first one if available
-      const imageUrl = publication.pictures.length > 0 ? publication.pictures[0].Url : PlaceholderImage;
+      const imageUrl =
+          publication.pictures.length > 0 ? publication.pictures[0].Url : PlaceholderImage;
+
+      let additionalInfoContent = '';
+
+      if (publication.rooms) {
+        additionalInfoContent += `<p>Rooms: ${publication.rooms}</p>`;
+      }
+
+      if (publication.livingArea) {
+        additionalInfoContent += `<p>Living area: ${publication.livingArea} m2</p>`;
+      }
 
       return `<div class="custom-popup">
-                <div class="popup-ribbon">
-                    ${publication.propertyCategory}
-                </div>
-                <img src="${imageUrl}" alt="" />
-                <div class="popup-content">
-                    <h3>${publication.publicationTitle}</h3>
-                    <p>${truncatedDescription}</p>
-                    <h2>${this.formatPrice(publication.price)}€</h2>
-                </div>
-               </div>`;
-      },
+            <div class="popup-ribbon">
+                ${publication.propertyCategory}
+            </div>
+            <div class="img-wrapper">
+                  <img src="${imageUrl}" alt="" />
+                  <div class="additional-info">
+                    ${additionalInfoContent}
+                  </div>
+            </div>
+            <div class="popup-content">
+                <h3>${publication.publicationTitle}</h3>
+                <p>${truncatedDescription}</p>
+                <h2>${this.formatPrice(publication.price)}€</h2>
+            </div>
+           </div>`;
+    },
+
     formatPrice(value) {
       let val = (value / 1).toFixed(2).replace('.', ',')
       return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
@@ -128,8 +136,8 @@ body {
 }
 
 .leaflet-container a.leaflet-popup-close-button {
-  right: 10px;
-  top: 10px;
+  right: 20px;
+  top: 20px;
   width: 30px;
   height: 30px;
   background: rgba(0,0,0,.5);
@@ -147,11 +155,12 @@ body {
 .custom-popup .popup-ribbon {
   background: #db0253;
   position: absolute;
-  top: 10px;
-  left: 10px;
+  top: 20px;
+  left: 20px;
   color: #fff;
   padding: 5px 10px;
   border-radius: 4px;
+  z-index: 2;
 }
 
 .custom-popup img {
@@ -164,5 +173,25 @@ body {
 
 .popup-content h3 {
   margin-top: 0;
+}
+
+.custom-popup .img-wrapper {
+  position: relative;
+}
+
+.custom-popup .img-wrapper .additional-info {
+  background: rgba(0,0,0, .6);
+  border-radius: 4px;
+  color: #fff;
+  position: absolute;
+  left: 20px;
+  bottom: 20px;
+  padding: 5px 10px;
+  display: flex;
+  gap: 20px;
+}
+
+.custom-popup .img-wrapper .additional-info p {
+  margin: 0;
 }
 </style>
